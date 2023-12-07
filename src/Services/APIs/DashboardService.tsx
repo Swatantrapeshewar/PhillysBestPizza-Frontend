99d8f11c28550df3b07adbe2129c59f4f6235c00
Swatantrapeshewar <swatantrapeshewar@gmail.com>
2023-12-07 21:49:58 +0530

    fix: dashboard-api-integration

--- src/Services/APIs/DashboardService.tsx
+++ src/Services/APIs/DashboardService.tsx
@@ -1,5 +1,7 @@
-// import axios from 'axios';
-// import { getAccessToken } from '../../Utils/helper';
+import axios from 'axios';
+import { getAccessToken } from '../../Utils/helper';
+
+const baseAPIURL = process.env.API_URL;
 
 export interface DashboardDetails {
 	totalItems: {
@@ -26,73 +28,73 @@ export interface DashboardResponse {
 	dasboardDetails: DashboardDetails;
 }
 
-const data: DashboardResponse = {
-	dasboardDetails: {
-		totalItems: {
-			count: '2',
-		},
-		totalCategories: {
-			count: '3',
-		},
-		itemsWithTotalStock: [
-			{
-				itemName: 'Coke',
-				availableQuantity: '10',
-				dailyConsumption: '0',
-				dailyThreshold: '10',
-				weeklyThreshold: '20',
-				overallThreshold: '30',
-				category: 'Beverages',
-				healthScore: 80,
-			},
-			{
-				itemName: 'Tomato Pizza',
-				availableQuantity: '27',
-				dailyConsumption: '13',
-				dailyThreshold: '10',
-				weeklyThreshold: '30',
-				overallThreshold: '20',
-				category: 'Pizza',
-				healthScore: 19.5,
-			},
-			{
-				itemName: 'Sprit',
-				availableQuantity: '10',
-				dailyConsumption: '0',
-				dailyThreshold: '10',
-				weeklyThreshold: '20',
-				overallThreshold: '30',
-				category: 'Beverages',
-				healthScore: 80,
-			},
-			{
-				itemName: 'Burgur',
-				availableQuantity: '27',
-				dailyConsumption: '13',
-				dailyThreshold: '10',
-				weeklyThreshold: '30',
-				overallThreshold: '20',
-				category: 'Pizza',
-				healthScore: 19.5,
-			},
-		],
-	},
-};
+// const data: DashboardResponse = {
+// 	dasboardDetails: {
+// 		totalItems: {
+// 			count: '2',
+// 		},
+// 		totalCategories: {
+// 			count: '3',
+// 		},
+// 		itemsWithTotalStock: [
+// 			{
+// 				itemName: 'Coke',
+// 				availableQuantity: '10',
+// 				dailyConsumption: '0',
+// 				dailyThreshold: '10',
+// 				weeklyThreshold: '20',
+// 				overallThreshold: '30',
+// 				category: 'Beverages',
+// 				healthScore: 80,
+// 			},
+// 			{
+// 				itemName: 'Tomato Pizza',
+// 				availableQuantity: '27',
+// 				dailyConsumption: '13',
+// 				dailyThreshold: '10',
+// 				weeklyThreshold: '30',
+// 				overallThreshold: '20',
+// 				category: 'Pizza',
+// 				healthScore: 19.5,
+// 			},
+// 			{
+// 				itemName: 'Sprit',
+// 				availableQuantity: '10',
+// 				dailyConsumption: '0',
+// 				dailyThreshold: '10',
+// 				weeklyThreshold: '20',
+// 				overallThreshold: '30',
+// 				category: 'Beverages',
+// 				healthScore: 80,
+// 			},
+// 			{
+// 				itemName: 'Burgur',
+// 				availableQuantity: '27',
+// 				dailyConsumption: '13',
+// 				dailyThreshold: '10',
+// 				weeklyThreshold: '30',
+// 				overallThreshold: '20',
+// 				category: 'Pizza',
+// 				healthScore: 19.5,
+// 			},
+// 		],
+// 	},
+// };
 
 export const getDeshboardDetails = async (): Promise<DashboardDetails> => {
-	// const AuthHeader = {
-	// 	headers: {
-	// 		Authorization: `${getAccessToken()}`,
-	// 	},
-	// };
-	// const response = axios
-	// 	.get(`${baseAPIURL}/inventory/item/list`, AuthHeader)
-	// 	.then(function (response) {
-	// 		return response.data.inventoryItemList;
-	// 	})
-	// 	.catch(function (error) {
-	// 		console.log(error);
-	// 		throw error;
-	// 	});
-	return data.dasboardDetails;
+	const AuthHeader = {
+		headers: {
+			Authorization: `${getAccessToken()}`,
+		},
+	};
+	const response = axios
+		.get(`${baseAPIURL}/dashboard/getDetails`, AuthHeader)
+		.then(function (response) {
+			return response.data.dasboardDetails;
+		})
+		.catch(function (error) {
+			console.log(error);
+			throw error;
+		});
+	return await response;
 };
